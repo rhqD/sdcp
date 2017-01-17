91f5bcc97063a1e701e5f2ecc4d687293cf45391
rhqD <eng-123@qq.com>
2017-01-17 09:17:50 +0800

    one missing file

--- sdcp/src/devices/device.java
+++ sdcp/src/devices/device.java
@@ -7,17 +7,23 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 
+import beans.request;
+import logger.logger;
+import java.util.regex.*;
 public class device implements Runnable{
 
 	private Socket dev;
 	private BufferedReader ins;
 	private OutputStream ous;
+	private boolean inited = false;
+	private int minutes = 0;
 	public device(Socket s){
 		dev = s;	
+		System.out.println("come on");
 		try {
 			dev.setReceiveBufferSize(4096);
 			dev.setSendBufferSize(4096);
-			dev.setSoTimeout(2000);
+			dev.setSoTimeout(1000);
 			ins = new BufferedReader(new InputStreamReader(s.getInputStream()));
 			ous = s.getOutputStream();
 		} catch (IOException e) {
@@ -28,6 +34,24 @@ public class device implements Runnable{
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
+	//进行device的初始化
+	private void init() throws IOException{
+	  request req = new request(this.ins);
+	  initWithRequest(req);
+	}
+	
+	private void initWithRequest(request req){
 		
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		try {
+			this.init();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
